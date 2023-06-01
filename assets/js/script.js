4ea00ec175f087628d1a3d502b9f4a2958dba5cb
JoseParada19 <131200876+JoseParada19@users.noreply.github.com>
2023-06-01 14:00:55 -0400

    recuperar cosas hacia la tabla
    aaa

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var registrar = ()=>{
+    let eContenedorTabla = document.getElementById("contenedorTabla");
     let eNombre = document.getElementById("nombre");
     let eApellido = document.getElementById("apellido");
     let nombre = eNombre.value;
@@ -18,6 +19,31 @@ var registrar = ()=>{
     console.log(listadoAntiguo)
     console.log(listadoNuevo);
     localStorage.setItem("personas",JSON.stringify(listadoNuevo));
+    //eContenedorTabla.innerHTML=""+listadoNuevo,length;
+    render = "<table>"
+    render+= "<tr><th>nombre</th><th>apellido</th><th>accion</th></tr>"
+    for (let i = 0; i < listadoNuevo.length; i++) {
+        const element= listadoNuevo[i];
+        console.log(element)
+        render+="<tr>"
+        render+="<td>"+element.nombre+"</td>"
+        render+="<td>"+element.apellido+"</td>"
+        render+="<td>"
+        render+="<button id='btnEditar"+i+"'>editar</button>"
+        render+="<button>eliminar</button>"
+        render+="</td>"
+        render+="</tr>"
+        
+    }   
+    render += "</table>";
+    eContenedorTabla.innerHTML = render;
+    for(let i = 0; i < listadoNuevo.length; i++){
+    var eBtn = document.getElementById("btnEditar"+i);
+    let element = listadoNuevo[i]
+    eBtn.addEventListener("click",()=>{alert("hola"+element.nombre+" "+element.apellido)})
+    }
+    
    }
+
 
 document.getElementById("btn").addEventListener("click",registrar);
